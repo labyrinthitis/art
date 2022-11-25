59ef943e46faf9e041fc61586c249e408b374cd2
apophatic <114704834+apophatic@users.noreply.github.com>
2022-11-24 18:57:52 -0800

    minor edits

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@ function request(keyword) {
       const $notfoundparagraph = document.createElement('p');
 
       $notfoundparagraph.className = 'txt-paragraph';
-      $notfoundparagraph.textContent = "There are no results for that criteria within the directory of the Harvard Art Mueseums. Try 'William Blake' or 'Dante Gabriel Rossetti'.";
+      $notfoundparagraph.textContent = "There are no results for that criteria within the directory of the Harvard Art Museums. Try 'William Blake', 'Dante Gabriel Rossetti', or 'Kahlil Gibran.";
 
       document.querySelector('.deck').append($notfoundparagraph);
 
